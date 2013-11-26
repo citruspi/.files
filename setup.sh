19fbdb2321cdbb1dc78c020216b1950e309c76a6
Mihir Singh <me@mihirsingh.com>
2013-11-26 07:23:45 -0500

    Removw and symlink zshrc

--- setup.sh
+++ setup.sh
@@ -4,12 +4,14 @@ git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 mkdir ~/.vim/colors
 wget -O ~/.vim/colors/Tomorrow-Night-Eighties.vim https://raw.github.com/chriskempson/vim-tomorrow-theme/master/colors/Tomorrow-Night-Eighties.vim
 
+rm -f ~/.zshrc
 rm -f ~/.vimrc
 rm -f ~/.bashrc
 rm -f ~/.gitconfig
 rm -f ~/.zlogin
 rm -f ~/.tmux.conf
 
+ln -s ~/.files/.zshrc ~/.zshrc
 ln -s ~/.files/.vimrc ~/.vimrc
 ln -s ~/.files/.bashrc ~/.bashrc
 ln -s ~/.files/.gitconfig ~/.gitconfig
