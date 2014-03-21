a3b5ae6be92ef20df0b264bce275004d2a1ad44a
Mihir Singh <me@mihirsingh.com>
2014-03-21 00:17:56 -0400

    Symlink global .gitignore

--- setup.sh
+++ setup.sh
@@ -15,6 +15,7 @@ ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.gitignore ~/.gitignore
 ln -s ~/dotfiles/.zlogin ~/.zlogin
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 
